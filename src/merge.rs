e84e8ed8b432871c232d7ab2e23b803eed6aa2c5
Brandon Sanderson <singingboyo@hotmail.com>
2013-09-03 23:03:31 -0700

    New merge function to properly merge issue comments.

--- src/merge.rs
+++ src/merge.rs
@@ -1,5 +1,5 @@
 use std::hashmap::HashMap;
-use issue::Issue;
+use issue::{Issue,IssueComment};
 pub fn mergeIssues(incoming:~[~Issue],mergeInto:~[~Issue]) -> ~[~Issue] {
   let mut identMap:HashMap<~str, (Option<~Issue>, Option<~Issue>)> = HashMap::new();
   for issue in incoming.move_iter() {
@@ -21,13 +21,33 @@ pub fn mergeIssues(incoming:~[~Issue],mergeInto:~[~Issue]) -> ~[~Issue] {
 }
 
 fn mergePair(issues:(Option<~Issue>, Option<~Issue>)) -> ~Issue {
-  let (incoming, mergeInto) = issues;
-  if(incoming.is_some() && mergeInto.is_some()){
-    //TODO actually merge
-    incoming.unwrap()
-  }else if(incoming.is_some()){
-    incoming.unwrap()
+  let (incomingOpt, mergeIntoOpt) = issues;
+  if(incomingOpt.is_some() && mergeIntoOpt.is_some()){
+    let incoming = incomingOpt.unwrap();
+    let mergeInto = mergeIntoOpt.unwrap();
+    let newComments = mergeComments(incoming.comments.clone(), 
+                                    mergeInto.comments.clone());
+    let status = incoming.status.clone();
+    ~Issue{comments:newComments, status:status, .. *incoming}
+  }else if(incomingOpt.is_some()){
+    incomingOpt.unwrap()
   }else{
-    mergeInto.unwrap()
+    mergeIntoOpt.unwrap()
   }
 }
+
+fn mergeComments(incoming:~[~IssueComment], mergeInto:~[~IssueComment]) -> ~[~IssueComment] {
+  let mut joined = incoming + mergeInto;
+  let mut merged:~[~IssueComment] = ~[];
+  while(joined.len() > 0) {
+    match joined.iter().min_by(|icomment| icomment.creationTime.to_timespec())
+                       .chain(|minimum| joined.position_elem(minimum)) {
+      Some(pos) => {
+        merged.push(joined.swap_remove(pos));
+      }
+      None => {}
+    }
+  }
+  merged
+}
+
