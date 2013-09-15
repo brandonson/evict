4b5818544c60d1040aa119348945e67385eebee4
Brandon Sanderson <singingboyo@hotmail.com>
2013-09-15 03:18:52 -0700

    Add license. (GPLv3)

--- src/commands/merge.rs
+++ src/commands/merge.rs
@@ -1,3 +1,21 @@
+/*
+ *   Copyright 2013 Brandon Sanderson
+ *
+ *   This file is part of Evict-BT.
+ *
+ *   Evict-BT is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Evict-BT is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Evict-BT.  If not, see <http://www.gnu.org/licenses/>.
+ */
 use std::io::{println};
 use file_manager;
 use file_manager::*;
